a5c748d0ec5231e0fbedf6c44aa4ccfd05aa3d2f
projeto-felipe-kenzo <79851474+projeto-felipe-kenzo@users.noreply.github.com>
2022-02-24 08:58:18 -0300

    Add files via upload

---
+++ aula_24_02_22/exemplo.php
@@ -0,0 +1,17 @@
+<?php
+    $numero = "10";
+
+    $outro_numero = "10";
+
+    var_dump($numero);
+    echo "<br>";
+    var_dump($outro_numero);
+    echo "<hr>";
+
+    if ($numero === $outro_numero){
+        echo "valores iguais";
+    }
+    else{
+        echo "são valores diferentes";
+    }
+?>
