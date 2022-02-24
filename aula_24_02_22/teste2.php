fd25465b3115a7ea49909faf089adbb88d6839ae
projeto-felipe-kenzo <79851474+projeto-felipe-kenzo@users.noreply.github.com>
2022-02-24 08:22:26 -0300

    Add files via upload

---
+++ aula_24_02_22/teste2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Teste Página PHP com HTML</h1>
+
+    <p><?php echo "<p class='oi'>Hello World!</p>";?>
+    </p>
+  
+</body>
+</html>
+
+<?php
+
+?>
